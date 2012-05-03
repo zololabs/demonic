1852efb94dd4f2a3a2924fabe37dc603cf036170
Amit Rathore <amitrathore@gmail.com>
2012-05-03 16:29:23 -0700

    added schema tracking

--- src/zolodeck/demonic/schema.clj
+++ src/zolodeck/demonic/schema.clj
@@ -1,14 +1,18 @@
 (ns zolodeck.demonic.schema
   (:use [datomic.api :only [tempid] :as db]))
 
+(def SCHEMA-MAP (atom {}))
+
 (defn fact-schema [attribute value-type cardinality fulltext? doc]
-  {:db/id (db/tempid :db.part/db)
-   :db/ident attribute
-   :db/valueType value-type
-   :db/cardinality :db.cardinality/one
-   :db/fulltext fulltext?
-   :db/doc doc
-   :db.install/_attribute :db.part/db})
+  (let [schema {:db/id (db/tempid :db.part/db)
+                :db/ident attribute
+                :db/valueType value-type
+                :db/cardinality :db.cardinality/one
+                :db/fulltext fulltext?
+                :db/doc doc
+                :db.install/_attribute :db.part/db}]
+    (swap! SCHEMA-MAP assoc attribute schema)
+    schema))
 
 (defn string-fact-schema [attribute fulltext? doc]
   (fact-schema attribute :db.type/string :db.cardinality/one fulltext? doc))
