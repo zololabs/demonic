d4d1c136e1612b9ba7612defa34940fcdee71fbf
Siva Jagadeesan <sivajag@gmail.com>
2012-05-11 20:31:53 -0700

    Revert "cleaning up loading nested entities"
    This reverts commit 973698cc0f9b4de3c48a351befee726a3cd165ff.
    
    This is not working

--- src/zolodeck/demonic/loadable.clj
+++ src/zolodeck/demonic/loadable.clj
@@ -4,26 +4,26 @@
 
 (deftype Loadable [m]
   clojure.lang.IPersistentMap
-  (assoc [this k v] (Loadable. (assoc m k v)))
+  (assoc [this k v] (Loadable. (atom (assoc @m k v))))
 
   clojure.lang.Counted
   (count [this]
-    (count m))
+    (count @m))
   
   clojure.lang.ILookup
   (valAt [this k]
-    (m k))
+    (get-value m k))
   (valAt [this k v]
-    (m k v))
+    (get-value m k v))
   
   clojure.lang.Seqable
-  (seq [this] (seq m))
+  (seq [this] (seq @m))
 
   clojure.lang.IFn
   (invoke [this] this)
-  (invoke [this k] (k m))
-  (invoke [this k v] (k m v))
+  (invoke [this k] (k @m))
+  (invoke [this k v] (k @m v))
   (applyTo [this args] (clojure.lang.AFn/applyToHelper this args)))
 
 (defn new-loadable [a-map]
-  (Loadable. a-map))
+  (Loadable. (atom a-map)))
