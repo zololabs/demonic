a0b901d08bafbca77bf6f51515b1b341b634f81c
Amit Rathore <amitrathore@gmail.com>
2013-02-12 13:03:45 -0800

    working on fixing tempid-resolution problem when creating + updating in the same demarcation

--- src/zolodeck/demonic/helper.clj
+++ src/zolodeck/demonic/helper.clj
@@ -1,5 +1,5 @@
 (ns zolodeck.demonic.helper
-  (:use [datomic.api :only [q db tempid squuid] :as db]
+  (:use [datomic.api :only [q db tempid squuid resolve-tempid] :as db]
         [zolodeck.demonic.schema :as schema]
         [zolodeck.utils.clojure :only [defrunonce random-guid diff]]
         [zolodeck.utils.maps :only [select-keys-if] :as maps]
@@ -10,6 +10,7 @@
 (def CONN)
 (def ^:dynamic TX-DATA)
 (def ^:dynamic DATOMIC-DB)
+(def ^:dynamic TEMP-IDS)
 (def ^:dynamic DATOMIC-TEST false)
 
 (defn setup-schema [schema-txs]
@@ -26,8 +27,22 @@
 (defrunonce initialize-datomic [datomic-db-name datomic-schema]
   (start-it-up- datomic-db-name datomic-schema))
 
-(defn next-temp-id []
-  (- (System/currentTimeMillis)))
+;; (defn next-temp-id []
+;;   (- (System/currentTimeMillis)))
+
+(defn get-new-temp-id []
+  (let [t (db/tempid :db.part/user)
+        temp-ids (-> TEMP-IDS
+                     deref
+                     last
+                     (assoc t nil))]
+    (reset! TEMP-IDS (conj (apply vector (butlast @TEMP-IDS)) temp-ids))
+    t))
+
+(defn pop-tempids []
+  (let [t (first @TEMP-IDS)]
+    (swap! TEMP-IDS rest)
+    t))
 
 (defn temp-db-id? [eid]
   (map? eid))
@@ -43,10 +58,10 @@
                  (and 
                   (schema/is-multiple-ref-attrib? attrib)
                   (sequential? value-or-values)) (map :db/id value-or-values)
-                  (and
-                   (schema/is-multiple-ref-attrib? attrib)
-                   (not (sequential? value-or-values))) (:db/id value-or-values)
-                   :else value-or-values)]
+                 (and
+                  (schema/is-multiple-ref-attrib? attrib)
+                  (not (sequential? value-or-values))) (:db/id value-or-values)
+                 :else value-or-values)]
       [:db/retract (:db/id entity) attrib value])))
 
 (defn retract-entity-txn [entity]
@@ -55,29 +70,55 @@
 (defn append-ref-txn [entity attrib value-entities]
   [:db/add (:db/id entity) attrib (map :db/id value-entities)])
 
+(defn update-with-realized-temp-ids [realized-tempids txn]
+  (print-vals "TXN:" txn)
+  (print-vals "Realized:" realized-tempids)  
+  (cond
+   :else txn))
+
+(defn update-all-with-realized-temp-ids [txns]
+  (let [realized (pop-tempids)]
+    (map #(update-with-realized-temp-ids realized %) txns)))
+
 (defn- datomic-transact [txns]
-  (let [tf (db/transact-async CONN txns)]
+  (let [txns (update-all-with-realized-temp-ids txns)
+        tf (db/transact-async CONN txns)]
     @tf))
 
-(defn speculative-transact [db txns]
-  (-> db
-      (db/with txns)
-      :db-after))
+;; (defn speculative-transact [db txns]
+;;   (-> db
+;;       (db/with txns)
+;;       :db-after))
+
+;; (defn update-db-speculatively [tx-data]
+    ;(swap! DATOMIC-DB speculative-transact tx-data)
+;; )
 
 (defn run-transaction [tx-data]
-  (swap! TX-DATA conj tx-data)
-  (swap! DATOMIC-DB speculative-transact tx-data))
+  (print-vals "Starting run-transaction...")
+  (let [{:keys [db-after tempids]} (db/with @DATOMIC-DB tx-data)
+        tempids-slice (print-vals "tempids-slice:" (last (print-vals "TEMP-IDS:" @TEMP-IDS)))
+        tempids-slice (reduce #(assoc %1 %2 (db/resolve-tempid db-after tempids %2))
+                              tempids-slice
+                              (keys tempids-slice))]
+    (reset! DATOMIC-DB db-after)
+    (swap! TX-DATA conj {:tx-data tx-data :temp-ids tempids-slice})
+    (swap! TEMP-IDS conj {}))
+  ;; (update-db-speculatively tx-data)
+  ;; (store-tx-data-for-later tx-data)
+  )
 
 (defn commit-pending-transactions []
   (when-not DATOMIC-TEST
     (when-not (empty? @TX-DATA)
-      (doseq [t @TX-DATA]
+      (doseq [{t :tx-data} @TX-DATA]
         (when-not (empty? t)
           (datomic-transact (doall t)))))))
 
 (defn run-in-demarcation [thunk]
   (binding [TX-DATA (atom [])
-            DATOMIC-DB (atom (db/db CONN))]
+            DATOMIC-DB (atom (db/db CONN))
+            TEMP-IDS (atom [{}{}])]
     (let [res (thunk)]
       (commit-pending-transactions)
       res)))
@@ -103,7 +144,7 @@
       (-> entity-or-map
           entity->map          
           (assoc gk (or (gk entity-or-map) (db/squuid)))
-          (assoc :db/id (or (:db/id entity-or-map) (db/tempid :db.part/user)))))))
+          (assoc :db/id (or (:db/id entity-or-map) (get-new-temp-id)))))))
 
 (defn schema-attrib-name [attrib-id]
   (-> '[:find ?a :in $ ?e :where [?e :db/ident ?a]]
